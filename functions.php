837a3ea50ac9205c46c02d14952baa2df8d565ca
Your Name <you@example.com>
2024-12-11 16:47:49 +0200

    Initialn commit

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function debug($data, $exit = false) {
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+
+    if ($exit) {
+        exit;
+    }
+}
+function is_admin(){
+return (isset($_SESSION['is_admin'])&& $_SESSION['is_admin']=='2');
+}
+?>
